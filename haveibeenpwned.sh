e6f499bd529f5c887941744c505aaaafc59ed058
Bergen Pohl <circidalia@gmail.com>
2022-02-23 22:45:46 -0500

    Added haveibeenpwned.sh

---
+++ haveibeenpwned.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n "Password: "
+read -s password
+echo ""
+
+hash=`echo -n $password | shasum | head -c 40`
+
+firstfive=${hash:0:5}
+everythingelse=${hash:5}
+
+res=$(curl -s "https://api.pwnedpasswords.com/range/$firstfive")
+count=`echo "$res" | grep -i "$everythingelse"`
+clean=`echo $count | sed 's/.*://'`
+
+echo $clean
